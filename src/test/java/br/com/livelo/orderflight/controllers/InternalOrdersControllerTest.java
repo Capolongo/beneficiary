a1a3b4d1af0c54c14f02b623db103c381dbe5710
fernandomonteiro <fernando.monteiro@compasso.com.br>
2024-01-03 11:22:16 -0300

    ORD-2 ajustando testes MockitoExtension.class

--- src/test/java/br/com/livelo/orderflight/controllers/InternalOrdersControllerTest.java
+++ src/test/java/br/com/livelo/orderflight/controllers/InternalOrdersControllerTest.java
@@ -6,11 +6,11 @@ import br.com.livelo.orderflight.utils.BaseTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -20,7 +20,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 @WebMvcTest(InternalOrdersController.class)
 public class InternalOrdersControllerTest {
     private String path = "/v1/internal/orders";
