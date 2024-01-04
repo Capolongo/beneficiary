fbf8a72d9fdf366f727926a4d3c3791abd3eb242
Felipe Monteiro <felipe.monteiro@livelo.com.br>
2024-01-04 10:37:09 -0300

    Ajuste nos cenários de testes

--- src/test/java/br/com/livelo/orderflight/controllers/InternalOrdersControllerTest.java
+++ src/test/java/br/com/livelo/orderflight/controllers/InternalOrdersControllerTest.java
@@ -1,43 +1,60 @@
 package br.com.livelo.orderflight.controllers;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import java.util.Optional;
-
+import br.com.livelo.orderflight.entities.OrderEntity;
+import br.com.livelo.orderflight.repository.OrderRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import br.com.livelo.orderflight.entities.OrderEntity;
-import br.com.livelo.orderflight.repository.OrderRepository;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 @ExtendWith(MockitoExtension.class)
-public class InternalOrdersControllerTest {
+class InternalOrdersControllerTest {
     @InjectMocks
     private InternalOrdersController controller;
     @Mock
     private OrderRepository orderRepository;
+    private OrderEntity order;
 
-    @Test
-    public void createOrderSuccess() throws Exception {
-        Mockito.when(orderRepository.save(Mockito.any(OrderEntity.class))).thenReturn(order(null).get());
-        var order = controller.createOrder(order(null).get());
-        assertNotNull(order);
+    @BeforeEach
+    void setup() {
+        order = OrderEntity.builder()
+                .id("lf1")
+                .build();
     }
 
     @Test
-    public void getOrderSuccess() throws Exception {
-        final String id = "lf1";
-        Mockito.when(orderRepository.findById(Mockito.anyString())).thenReturn(order(id));
-        var order = controller.getById(id);
-        assertNotNull(order);
+    public void shouldCreateOrder() throws Exception {
+        var expected = mock(OrderEntity.class);
+        Mockito.when(orderRepository.save(Mockito.any(OrderEntity.class))).thenReturn(expected);
+
+        var response = controller.createOrder(expected);
+        assertAll(
+                () -> assertTrue(response.getStatusCode().is2xxSuccessful()),
+                () -> assertNotNull(response),
+                () -> assertEquals(expected, response.getBody())
+        );
     }
 
-    private Optional<OrderEntity> order(String id) {
-        return Optional.of(OrderEntity.builder().commerceOrderId("1").originOrder("1").id(id).partnerOrderId("1").channel("1").customerIdentifier("1")
-                .build());
+    //TODO ESCREVER TESTES PARA CENÁRIOS DE ERRO
+
+    @Test
+    public void shouldRetrieveOrder() {
+        Mockito.when(orderRepository.findById(Mockito.anyString())).thenReturn(Optional.of(order));
+
+        var response = controller.getById(order.getId());
+        assertAll(
+                () -> assertTrue(response.getStatusCode().is2xxSuccessful()),
+                () -> assertNotNull(response),
+                () -> assertEquals(order, response.getBody())
+        );
     }
 }
 
