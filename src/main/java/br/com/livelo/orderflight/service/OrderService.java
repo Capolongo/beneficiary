fce42207266531ef1c747e089660d8c8c55e541b
Felipe Lanzarin <felipe.lanzarin@livelo.com.br>
2024-01-08 12:10:51 -0300

    Configurações iniciais.

--- src/main/java/br/com/livelo/orderflight/service/OrderService.java
+++ src/main/java/br/com/livelo/orderflight/service/OrderService.java
@@ -1,7 +1,22 @@
 package br.com.livelo.orderflight.service;
 
+import br.com.livelo.orderflight.domain.entity.OrderEntity;
+import br.com.livelo.orderflight.repository.OrderRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@AllArgsConstructor
 public class OrderService {
+
+    private OrderRepository orderRepository;
+    public OrderEntity getOrder(String id) throws Exception {
+        Optional<OrderEntity> op = orderRepository.findById(id);
+        if(op.isPresent()){
+            return op.get();
+        }
+        throw new Exception("Order NotFount");
+    }
 }
--- src/main/java/br/com/livelo/orderflight/service/checkout/CheckoutService.java
+++ src/main/java/br/com/livelo/orderflight/service/checkout/CheckoutService.java
@@ -1,8 +1,35 @@
 package br.com.livelo.orderflight.service.checkout;
 
 
+import br.com.livelo.orderflight.domain.entity.OrderEntity;
+import br.com.livelo.orderflight.service.OrderService;
+import br.com.livelo.partnersconfigflightlibrary.dto.WebhookDTO;
+import br.com.livelo.partnersconfigflightlibrary.utils.Webhooks;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import br.com.livelo.partnersconfigflightlibrary.services.PartnersConfigService;
 
 @Service
+@AllArgsConstructor
 public class CheckoutService {
+    private PartnersConfigService partnersConfigService;
+    private OrderService orderService;
+    public void confirmOrder(String partnerCode) throws Exception {
+        OrderEntity order = orderService.getOrder("");
+        validateRequest();
+        confirmOnPartner(partnerCode);
+        //salva informacoes no OrderService
+        //retorna resultado
+    }
+
+    private void validateRequest(){
+
+    }
+
+    private void confirmOnPartner(String partnerCode){
+
+        WebhookDTO webhook = partnersConfigService.getPartnerWebhook(partnerCode, Webhooks.CONFIRMATION);
+        // chama proxy de confirmação
+        // retorna resultado por meio do status
+    }
 }
