b0c72b9ea766c06e31e2de26f12f7004b37b4309
T01400 <renan.melo@compasso.com.br>
2024-01-17 10:30:24 -0300

    feat: adicionando mapper de status

--- src/main/java/br/com/livelo/orderflight/mappers/ReservationMapper.java
+++ src/main/java/br/com/livelo/orderflight/mappers/ReservationMapper.java
@@ -4,6 +4,7 @@ import br.com.livelo.orderflight.domain.dto.*;
 import br.com.livelo.orderflight.domain.entity.OrderEntity;
 import br.com.livelo.orderflight.domain.entity.OrderItemEntity;
 import br.com.livelo.orderflight.domain.entity.OrderPriceEntity;
+import br.com.livelo.orderflight.domain.entity.OrderStatusEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
@@ -28,7 +29,7 @@ public interface ReservationMapper {
     @Mapping(target = "customerIdentifier", source = "customerId")
     //    TODO entender o status e o statusHistory
     @Mapping(target = "statusHistory", ignore = true)
-    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "status", expression="java(mapStatus(partnerReservationResponse))")
     @Mapping(target = "price", expression = "java(mapPrice(partnerReservationResponse, listPrice))")
     OrderEntity toOrderEntity(ReservationRequest cartRequest, PartnerReservationResponse partnerReservationResponse, String transactionId, String customerId, String channel, String listPrice);
 
@@ -36,6 +37,11 @@ public interface ReservationMapper {
         ReservationPriceMapper reservationPriceMapper = Mappers.getMapper(ReservationPriceMapper.class);
 
         return reservationPriceMapper.toOrderPriceEntity(partnerReservationResponse, listPrice);
+    }
+
+    default OrderStatusEntity mapStatus(PartnerReservationResponse partnerReservationResponse) {
+        ReservationStatusMapper reservationStatusMapper = Mappers.getMapper(ReservationStatusMapper.class);
+        return reservationStatusMapper.toOrderStatus(partnerReservationResponse);
     }
 
     default Set<OrderItemEntity> mapItems(ReservationRequest cartRequest, PartnerReservationResponse partnerReservationResponse) {
