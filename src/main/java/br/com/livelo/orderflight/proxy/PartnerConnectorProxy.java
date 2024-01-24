222bb05515771d3249264b07b592697f05a9d332
T01400 <renan.melo@compasso.com.br>
2024-01-24 14:02:00 -0300

    Ajustando Retry para usar notation

--- src/main/java/br/com/livelo/orderflight/proxy/PartnerConnectorProxy.java
+++ src/main/java/br/com/livelo/orderflight/proxy/PartnerConnectorProxy.java
@@ -3,6 +3,7 @@ package br.com.livelo.orderflight.proxy;
 import br.com.livelo.orderflight.client.Constants;
 import br.com.livelo.orderflight.client.PartnerConnectorClient;
 import br.com.livelo.orderflight.config.PartnerProperties;
+//import br.com.livelo.orderflight.config.RetryConditionEvaluator;
 import br.com.livelo.orderflight.domain.dto.reservation.request.PartnerReservationRequest;
 import br.com.livelo.orderflight.domain.dto.reservation.response.PartnerReservationResponse;
 import br.com.livelo.orderflight.exception.ReservationException;
@@ -12,6 +13,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -25,41 +28,55 @@ import static java.util.Optional.ofNullable;
 @Service
 @RequiredArgsConstructor
 @Slf4j
+
 public class PartnerConnectorProxy {
     private final PartnerConnectorClient partnerConnectorClient;
     private final PartnerProperties partnerProperties;
+//    private final RetryConditionEvaluator retryConditionEvaluator;
 
-    public PartnerReservationResponse reservation(PartnerReservationRequest request, String transactionId) {
+
+    @Retryable(retryFor = FeignException.class, maxAttemptsExpression = "#{#attempt}")
+    public PartnerReservationResponse reservation(int attempt, PartnerReservationRequest request, String transactionId) {
         try {
+//            RetryTemplate retryTemplate = retryConditionEvaluator.createRetryTemplate(request.getPartnerCode());
+
+//            return retryTemplate.execute(retryContext -> {
+                log.info("FUIIII CHAMADOOOOO");
             var response = partnerConnectorClient.reservation(
                     getUrlByPartnerCode(request.getPartnerCode()),
                     request,
                     getHeaders(Collections.singletonMap(Constants.TRANSACTION_ID, transactionId)));
 
             return this.handleResponse(response);
+//            });
+
         } catch (ReservationException e) {
             throw e;
-        } catch (FeignException e) {
-            var status = HttpStatus.valueOf(e.status());
-            if (status.is5xxServerError()) {
-                throw new ReservationException(
-                        ReservationErrorType.FLIGHT_CONNECTOR_INTERNAL_ERROR,
-                        null,
-                        "Erro interno ao se comunicar com parceiro no conector. ResponseBody: " + e.responseBody().toString(),
-                        e
-                );
-            } else {
-                throw new ReservationException(
-                        ReservationErrorType.FLIGHT_CONNECTOR_BUSINESS_ERROR,
-                        null,
-                        "Erro interno ao se comunicar com parceiro no conector. ResponseBody: " + e.responseBody().toString(),
-                        e
-                );
-            }
-        } catch (Exception e) {
-            throw new ReservationException(ReservationErrorType.ORDER_FLIGHT_INTERNAL_ERROR, e.getMessage(), null, e);
+//        } catch (FeignException e) {
+////            var status = HttpStatus.valueOf(e.status());
+////            if (status.is5xxServerError()) {
+////                throw new ReservationException(
+////                        ReservationErrorType.FLIGHT_CONNECTOR_INTERNAL_ERROR,
+////                        null,
+////                        "Erro interno ao se comunicar com parceiro no conector. ResponseBody: " + e.responseBody().toString(),
+////                        e
+////                );
+////
+////            } else {
+////                throw new ReservationException(
+////                        ReservationErrorType.FLIGHT_CONNECTOR_BUSINESS_ERROR,
+////                        null,
+////                        "Erro interno ao se comunicar com parceiro no conector. ResponseBody: " + e.responseBody().toString(),
+////                        e
+////                );
+////            }
+//        } catch (Exception e) {
+//            throw new ReservationException(ReservationErrorType.ORDER_FLIGHT_INTERNAL_ERROR, e.getMessage(), null, e);
+//        }
         }
+
     }
+
 
     private MultiValueMap<String, String> getHeaders(Map<String, String> mapHeaders) {
         MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
@@ -82,4 +99,5 @@ public class PartnerConnectorProxy {
     private URI getUrlByPartnerCode(String partnerCode) {
         return URI.create(partnerProperties.getUrlByPartnerCode(partnerCode));
     }
+
 }
