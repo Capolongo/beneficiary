b008e029e2a17820ed3829732c894e5ec2bd4b7c
mariomendonca <mario.neto@taking.com.br>
2024-01-29 17:46:44 -0300

    test: create tests to service

--- src/test/java/br/com/livelo/orderflight/mock/MockBuilder.java
+++ src/test/java/br/com/livelo/orderflight/mock/MockBuilder.java
@@ -4,8 +4,10 @@ import br.com.livelo.orderflight.domain.dtos.confirmation.request.ConfirmOrderIt
 import br.com.livelo.orderflight.domain.dtos.confirmation.request.ConfirmOrderPriceRequest;
 import br.com.livelo.orderflight.domain.dtos.confirmation.request.ConfirmOrderRequest;
 import br.com.livelo.orderflight.domain.dtos.confirmation.response.ConfirmOrderResponse;
+import br.com.livelo.orderflight.domain.entity.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -57,6 +59,129 @@ public class MockBuilder {
                 .status(null)
                 .price(null)
                 .items(null)
+                .build();
+    }
+
+    public static OrderEntity orderEntity() {
+        Set<OrderItemEntity> items = new HashSet<>();
+        items.add(orderItemEntity());
+
+        Set<OrderStatusEntity> statusHistory = new HashSet<>();
+        statusHistory.add(statusInitial());
+
+        return OrderEntity.builder()
+                .id("id")
+                .commerceOrderId("commerceOrderId")
+                .partnerOrderId("partnerOrderId")
+                .partnerCode("partnerCode")
+                .submittedDate(LocalDateTime.now())
+                .channel("channel")
+                .tierCode("tierCode")
+                .originOrder("originOrder")
+                .customerIdentifier("customerIdentifier")
+                .transactionId("transactionId")
+                .expirationDate(LocalDateTime.now())
+                .price(orderPriceEntity())
+                .items(items)
+                .statusHistory(statusHistory)
+                .currentStatus(statusInitial())
+                .build();
+    }
+
+    public static OrderItemEntity orderItemEntity() {
+        Set<SegmentEntity> segments = new HashSet<>();
+        segments.add(segmentEntity());
+
+        return OrderItemEntity.builder()
+                .id(1L)
+                .commerceItemId("commerceItemId")
+                .skuId("skuId")
+                .productId("productId")
+                .quantity(1)
+                .externalCoupon("externalCoupon")
+                .price(orderItemPriceEntity())
+                .travelInfo(travelInfo())
+                .segments(segments)
+                .build();
+    }
+
+    public static OrderItemPriceEntity orderItemPriceEntity() {
+        return OrderItemPriceEntity.builder()
+                .id(1L)
+                .listPrice("listPrice")
+                .amount(BigDecimal.valueOf(1000))
+                .pointsAmount(BigDecimal.valueOf(1000))
+                .accrualPoints(BigDecimal.valueOf(1000))
+                .partnerAmount(BigDecimal.valueOf(1000))
+                .priceListId("priceListId")
+                .build();
+    }
+
+    public static TravelInfoEntity travelInfo() {
+        return TravelInfoEntity.builder().build();
+    }
+
+    public static SegmentEntity segmentEntity() {
+        return SegmentEntity.builder().build();
+    }
+
+    public static OrderPriceEntity orderPriceEntity() {
+        Set<OrderPriceDescriptionEntity> orderPriceDescriptions = new HashSet<>();
+        orderPriceDescriptions.add(orderPriceDescriptionEntity());
+        return OrderPriceEntity.builder()
+                .id(1L)
+                .accrualPoints(1000.0)
+                .amount(BigDecimal.valueOf(1000))
+                .pointsAmount(BigDecimal.valueOf(1000))
+                .partnerAmount(BigDecimal.valueOf(1000))
+                .priceListId("priceListId")
+                .ordersPriceDescription(orderPriceDescriptions)
+                .build();
+    }
+
+    public static OrderPriceDescriptionEntity orderPriceDescriptionEntity() {
+        return OrderPriceDescriptionEntity.builder()
+                .id(1L)
+                .amount(BigDecimal.valueOf(1000))
+                .pointsAmount(BigDecimal.valueOf(1000))
+                .type("type")
+                .description("desc")
+                .build();
+    }
+
+    public static OrderStatusEntity statusInitial() {
+        return OrderStatusEntity.builder()
+                .id(1L)
+                .code("LIVPNR-1006")
+                .description("desc")
+                .partnerCode("partnerCode")
+                .partnerDescription("desc")
+                .partnerResponse("response")
+                .statusDate(LocalDateTime.now())
+                .build();
+    }
+
+    public static OrderStatusEntity statusProcessing() {
+        return OrderStatusEntity.builder()
+                .id(1L)
+                .code("LIVPNR-1007")
+                .description("desc")
+                .partnerCode("partnerCode")
+                .partnerDescription("desc")
+                .partnerResponse("response")
+                .statusDate(LocalDateTime.now())
+                .build();
+    }
+
+    public static OrderStatusEntity statusFailed() {
+        return OrderStatusEntity.builder()
+                .id(1L)
+                .code("LIVPNR-1014")
+                .description("desc")
+                .partnerCode("partnerCode")
+                .partnerDescription("desc")
+                .partnerResponse("response")
+                .statusDate(LocalDateTime.now())
                 .build();
     }
 }
--- src/test/java/br/com/livelo/orderflight/service/confirmation/ConfirmationServiceTest.java
+++ src/test/java/br/com/livelo/orderflight/service/confirmation/ConfirmationServiceTest.java
@@ -1,6 +1,10 @@
 package br.com.livelo.orderflight.service.confirmation;
 
+import br.com.livelo.orderflight.domain.dtos.confirmation.response.ConfirmOrderResponse;
+import br.com.livelo.orderflight.domain.dtos.connector.request.ConnectorConfirmOrderRequest;
+import br.com.livelo.orderflight.domain.dtos.connector.response.ConnectorConfirmOrderResponse;
 import br.com.livelo.orderflight.mapper.ConfirmOrderMapper;
+import br.com.livelo.orderflight.mock.MockBuilder;
 import br.com.livelo.orderflight.proxies.ConnectorPartnersProxy;
 import br.com.livelo.orderflight.repository.OrderRepository;
 import br.com.livelo.orderflight.service.OrderService;
@@ -9,8 +13,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 class ConfirmationServiceTest {
@@ -27,6 +33,14 @@ class ConfirmationServiceTest {
     private ConfirmationService confirmationService;
 
     @Test
-    void shouldValidateIfPartnerOrdersIdsAreEqual() {
+    void shouldConfirmOrder() throws Exception {
+        when(orderService.getOrderById(anyString())).thenReturn(MockBuilder.orderEntity());
+        when(connectorPartnersProxy.confirmOnPartner(anyString(), any(ConnectorConfirmOrderRequest.class))).thenReturn(any(ConnectorConfirmOrderResponse.class));
+
+        ConfirmOrderResponse confirmOrderResponse = confirmationService.confirmOrder("id", MockBuilder.confirmOrderRequest());
+
+        assertEquals(MockBuilder.confirmOrderResponse(), confirmOrderResponse);
+
+
     }
 }
