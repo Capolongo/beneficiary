8f84d8f3e0b649ceec69d08fabaf92671c34f996
Kaê Benning <kae.leal@taking.com.br>
2024-02-15 11:57:38 -0300

    feat: unit tests api

--- src/test/java/br/com/livelo/orderflight/service/order/OrderServiceTest.java
+++ src/test/java/br/com/livelo/orderflight/service/order/OrderServiceTest.java
@@ -1,7 +1,10 @@
-package br.com.livelo.orderflight.service;
+package br.com.livelo.orderflight.service.order;
 
 import br.com.livelo.orderflight.mappers.ConfirmOrderMapper;
+import br.com.livelo.orderflight.mappers.OrderProcessMapper;
 import br.com.livelo.orderflight.domain.dtos.connector.response.ConnectorConfirmOrderStatusResponse;
+import br.com.livelo.orderflight.domain.dtos.repository.OrderProcess;
+import br.com.livelo.orderflight.domain.dtos.repository.PaginationOrderProcessResponse;
 import br.com.livelo.orderflight.domain.entity.OrderEntity;
 import br.com.livelo.orderflight.domain.entity.OrderStatusEntity;
 import br.com.livelo.orderflight.exception.OrderFlightException;
@@ -20,6 +23,10 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.Optional;
 
@@ -29,6 +36,8 @@ class OrderServiceTest {
     private OrderRepository orderRepository;
     @Mock
     private ConfirmOrderMapper confirmOrderMapper;
+    @Mock
+    private OrderProcessMapper orderProcessMapper;
 
     @InjectMocks
     private OrderServiceImpl orderService;
@@ -101,5 +110,27 @@ class OrderServiceTest {
         var response = this.orderService.save(orderMock);
 
         assertNotNull(response);
+    }
+
+    @Test
+    void shouldReturnSuccessGetOrdersByStatusCode() throws OrderFlightException {
+        ReflectionTestUtils.setField(orderService, "orderProcessMaxRows", 500);
+
+        String statusCode = "LIVPNR-1006";
+        int page = 0;
+        int rows = 4;
+
+        Page<OrderProcess> repositoryResponse = Page.empty();
+        PaginationOrderProcessResponse mappedRepositoryResponse = MockBuilder.paginationOrderProcessResponse(page, rows);
+
+        when(orderRepository.findAllByCurrentStatusCode(anyString(), any(Pageable.class))).thenReturn(repositoryResponse);
+        when(orderProcessMapper.pageRepositoryToPaginationResponse(any())).thenReturn(mappedRepositoryResponse);
+
+        PaginationOrderProcessResponse response = orderService.getOrdersByStatusCode(statusCode, page, rows);
+
+        assertEquals(mappedRepositoryResponse, response);
+        assertEquals(mappedRepositoryResponse.getOrders().size(), response.getRows());
+        verify(orderRepository).findAllByCurrentStatusCode(statusCode, PageRequest.of(page, rows));
+        verifyNoMoreInteractions(orderRepository);
     }
 }
