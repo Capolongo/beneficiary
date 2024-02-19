d3a06a7451b89a76135427e20681fa1936d66514
Kaê Benning <kae.leal@taking.com.br>
2024-02-19 14:53:13 -0300

    test: increment more one test

--- src/test/java/br/com/livelo/orderflight/mappers/OrderProcessMapperTest.java
+++ src/test/java/br/com/livelo/orderflight/mappers/OrderProcessMapperTest.java
@@ -1,6 +1,12 @@
 package br.com.livelo.orderflight.mappers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -30,6 +36,13 @@ class OrderProcessMapperTest {
     PaginationOrderProcessResponse mappedValues = mapper.pageRepositoryToPaginationResponse(rawPageOrderProcess);
 
     assertEquals(MockBuilder.paginationOrderProcessResponse(1, rows), mappedValues);
+  }
+
+  @Test
+  void shouldValidateEmptyOrderProcessList() {
+    List<OrderProcess> mappedValues = mapper.contentToOrderProcess(List.of());
+
+    assertEquals(0, mappedValues.size());
   }
 
 }
