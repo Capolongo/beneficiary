1b57c6532b4dcd880a887f8e284bfae635becf96
Felipe Monteiro <felipe.monteiro@livelo.com.br>
2024-02-19 10:16:29 -0300

    Ajustes sonar

--- src/main/java/br/com/livelo/orderflight/mappers/ReservationPriceMapper.java
+++ src/main/java/br/com/livelo/orderflight/mappers/ReservationPriceMapper.java
@@ -1,22 +1,23 @@
 package br.com.livelo.orderflight.mappers;
 
+import br.com.livelo.orderflight.domain.dto.reservation.response.PartnerReservationOrdersPriceDescription;
+import br.com.livelo.orderflight.domain.dto.reservation.response.PartnerReservationOrdersPriceDescriptionTaxes;
+import br.com.livelo.orderflight.domain.dto.reservation.response.PartnerReservationResponse;
+import br.com.livelo.orderflight.domain.dtos.pricing.response.PricingCalculatePrice;
+import br.com.livelo.orderflight.domain.dtos.pricing.response.PricingCalculateTaxes;
+import br.com.livelo.orderflight.domain.entity.OrderPriceDescriptionEntity;
+import br.com.livelo.orderflight.domain.entity.OrderPriceEntity;
+import br.com.livelo.orderflight.exception.OrderFlightException;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
 import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-import br.com.livelo.orderflight.domain.dtos.pricing.response.PricingCalculatePricesDescription;
-import br.com.livelo.orderflight.domain.dtos.pricing.response.PricingCalculateTaxes;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
-
-import br.com.livelo.orderflight.domain.dto.reservation.response.PartnerReservationOrdersPriceDescription;
-import br.com.livelo.orderflight.domain.dto.reservation.response.PartnerReservationOrdersPriceDescriptionTaxes;
-import br.com.livelo.orderflight.domain.dto.reservation.response.PartnerReservationResponse;
-import br.com.livelo.orderflight.domain.dtos.pricing.response.PricingCalculatePrice;
-import br.com.livelo.orderflight.domain.entity.OrderPriceDescriptionEntity;
-import br.com.livelo.orderflight.domain.entity.OrderPriceEntity;
+import static br.com.livelo.orderflight.exception.enuns.OrderFlightErrorType.ORDER_FLIGHT_INTERNAL_ERROR;
 
 @Mapper(componentModel = "spring", uses = ReservationOrderPriceDescriptionMapper.class)
 public interface ReservationPriceMapper {
@@ -29,29 +30,37 @@ public interface ReservationPriceMapper {
     @Mapping(target = "ordersPriceDescription", expression = "java(mapOrdersPriceDescription(partnerReservationResponse,pricingCalculatePrice))")
     OrderPriceEntity toOrderPriceEntity(PartnerReservationResponse partnerReservationResponse, String listPrice, PricingCalculatePrice pricingCalculatePrice);
 
-    default Set<OrderPriceDescriptionEntity> mapOrdersPriceDescription(
-            PartnerReservationResponse partnerReservationResponse,PricingCalculatePrice pricingCalculatePrice) {
-        
-    	var orderPriceDescriptionMapper = Mappers.getMapper(ReservationOrderPriceDescriptionMapper.class);
+    default Set<OrderPriceDescriptionEntity> mapOrdersPriceDescription(PartnerReservationResponse partnerReservationResponse, PricingCalculatePrice pricingCalculatePrice) {
+        var orderPriceDescriptionMapper = Mappers.getMapper(ReservationOrderPriceDescriptionMapper.class);
         var orderPriceDescriptionTaxesMapper = Mappers.getMapper(ReservationOrderPriceDescriptionTaxesMapper.class);
-        
+
         if (partnerReservationResponse.getOrdersPriceDescription() != null) {
-        	Set<OrderPriceDescriptionEntity> ordersPriceDescription = new HashSet<>();
-        	
-        	for(PartnerReservationOrdersPriceDescription partnerReservationOrdersPriceDescription:partnerReservationResponse.getOrdersPriceDescription()) {
+            Set<OrderPriceDescriptionEntity> ordersPriceDescription = new HashSet<>();
+
+            for (PartnerReservationOrdersPriceDescription partnerReservationOrdersPriceDescription : partnerReservationResponse.getOrdersPriceDescription()) {
                 OrderPriceDescriptionEntity orderPriceDescriptionEntity = orderPriceDescriptionMapper.toOrderPriceDescriptionEntity(partnerReservationOrdersPriceDescription);
-                PricingCalculatePricesDescription pricingCalculatePricesDescription = pricingCalculatePrice.getPricesDescription()
-                        .stream().filter(priceDescription -> partnerReservationOrdersPriceDescription.getType().equals(priceDescription.getPassengerType())).findFirst().get();
-                orderPriceDescriptionEntity.setPointsAmount(new BigDecimal(pricingCalculatePricesDescription.getPointsAmount()));
-        		ordersPriceDescription.add(orderPriceDescriptionEntity);
-        		for(PartnerReservationOrdersPriceDescriptionTaxes partnerReservationOrdersPriceDescriptionTaxes:partnerReservationOrdersPriceDescription.getTaxes()) {
+
+                var pricingCalculatePricesDescription = pricingCalculatePrice.getPricesDescription()
+                        .stream()
+                        .filter(priceDescription -> partnerReservationOrdersPriceDescription.getType().equals(priceDescription.getPassengerType()))
+                        .findFirst()
+                        .orElseThrow(() -> new OrderFlightException(ORDER_FLIGHT_INTERNAL_ERROR, null, "Passenger type not found"));
+
+                orderPriceDescriptionEntity.setPointsAmount(BigDecimal.valueOf(pricingCalculatePricesDescription.getPointsAmount()));
+                ordersPriceDescription.add(orderPriceDescriptionEntity);
+
+                for (PartnerReservationOrdersPriceDescriptionTaxes partnerReservationOrdersPriceDescriptionTaxes : partnerReservationOrdersPriceDescription.getTaxes()) {
                     OrderPriceDescriptionEntity orderPriceDescriptionEntityTaxes = orderPriceDescriptionTaxesMapper.toOrderPriceDescriptionEntity(partnerReservationOrdersPriceDescriptionTaxes);
+
                     PricingCalculateTaxes pricingCalculateTaxes = pricingCalculatePricesDescription.getTaxes()
-                            .stream().filter(priceDescriptionTaxes -> orderPriceDescriptionEntityTaxes.getDescription().equals(priceDescriptionTaxes.getDescription())).findFirst().get();
-                    orderPriceDescriptionEntityTaxes.setPointsAmount(new BigDecimal(pricingCalculateTaxes.getPointsAmount()));
-        			ordersPriceDescription.add(orderPriceDescriptionEntityTaxes);
-        		}
-        	}
+                            .stream().filter(priceDescriptionTaxes -> orderPriceDescriptionEntityTaxes.getDescription().equals(priceDescriptionTaxes.getDescription()))
+                            .findFirst()
+                            .orElseThrow(() -> new OrderFlightException(ORDER_FLIGHT_INTERNAL_ERROR, null, "Tax description not found"));
+
+                    orderPriceDescriptionEntityTaxes.setPointsAmount(BigDecimal.valueOf(pricingCalculateTaxes.getPointsAmount()));
+                    ordersPriceDescription.add(orderPriceDescriptionEntityTaxes);
+                }
+            }
             return ordersPriceDescription;
         }
         return Collections.emptySet();
