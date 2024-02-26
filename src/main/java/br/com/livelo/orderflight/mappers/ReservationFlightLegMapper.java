0054799bd109f6d5ee053fc017938a6234edb85a
Felipe Monteiro <felipe.monteiro@livelo.com.br>
2024-02-26 15:01:13 -0300

    ajustes nos testes unitários

--- src/main/java/br/com/livelo/orderflight/mappers/ReservationFlightLegMapper.java
+++ src/main/java/br/com/livelo/orderflight/mappers/ReservationFlightLegMapper.java
@@ -3,8 +3,21 @@ package br.com.livelo.orderflight.mappers;
 import br.com.livelo.orderflight.domain.dto.reservation.response.PartnerReservationFlightsLeg;
 import br.com.livelo.orderflight.domain.entity.FlightLegEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ReservationFlightLegMapper {
+    @Mapping(target = "flightNumber", source = "flightNumber")
+    @Mapping(target = "flightDuration", source = "flightDuration")
+    @Mapping(target = "airline", source = "operatedBy")
+    @Mapping(target = "managedBy", source = "managedBy")
+    @Mapping(target = "timeToWait", source = "timeToWait")
+    @Mapping(target = "originIata", source = "originIata")
+    @Mapping(target = "originDescription", source = "originDescription")
+    @Mapping(target = "destinationIata", source = "destinationIata")
+    @Mapping(target = "destinationDescription", source = "destinationDescription")
+    @Mapping(target = "departureDate", source = "departureDate")
+    @Mapping(target = "arrivalDate", source = "arrivalDate")
+    @Mapping(target = "type", source = "type")
     FlightLegEntity toFlightLegEntity(PartnerReservationFlightsLeg partnerReservationFlightsLeg);
 }
--- src/test/java/br/com/livelo/orderflight/mappers/ReservationPriceMapperTest.java
+++ src/test/java/br/com/livelo/orderflight/mappers/ReservationPriceMapperTest.java
@@ -1,47 +1,54 @@
 package br.com.livelo.orderflight.mappers;
 
-import br.com.livelo.orderflight.domain.dto.reservation.response.*;
+import br.com.livelo.orderflight.domain.dto.reservation.response.PartnerReservationOrdersPriceDescription;
+import br.com.livelo.orderflight.domain.dto.reservation.response.PartnerReservationOrdersPriceDescriptionFlight;
+import br.com.livelo.orderflight.domain.dto.reservation.response.PartnerReservationOrdersPriceDescriptionTaxes;
+import br.com.livelo.orderflight.domain.dto.reservation.response.PartnerReservationResponse;
 import br.com.livelo.orderflight.domain.dtos.pricing.response.PricingCalculateFlight;
 import br.com.livelo.orderflight.domain.dtos.pricing.response.PricingCalculatePrice;
 import br.com.livelo.orderflight.domain.dtos.pricing.response.PricingCalculatePricesDescription;
 import br.com.livelo.orderflight.domain.dtos.pricing.response.PricingCalculateTaxes;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(MockitoExtension.class)
 class ReservationPriceMapperTest {
-    ReservationPriceMapper mapper = new ReservationPriceMapperImpl();
+    ReservationPriceMapper mapper = Mappers.getMapper(ReservationPriceMapper.class);
 
     @Test
     void shouldReturnOrdersPriceDescription() {
         var response = this.mapper.mapOrdersPriceDescription(PartnerReservationResponse.builder()
-                .ordersPriceDescription(
-                        List.of(PartnerReservationOrdersPriceDescription.builder()
-                                .amount(new BigDecimal(10))
-                                .type("BY_ADULT")
-                                .taxes(
-                                        List.of(PartnerReservationOrdersPriceDescriptionTaxes.builder()
-                                                .amount(new BigDecimal(10))
-                                                .description("TESTE_TAX")
-                                                .build())).build())
-                ).build(),
+                        .ordersPriceDescription(
+                                PartnerReservationOrdersPriceDescription.builder()
+                                        .flights(List.of(
+                                                PartnerReservationOrdersPriceDescriptionFlight.builder()
+                                                        .amount(new BigDecimal(10))
+                                                        .passengerType("BY_ADULT")
+                                                        .build()
+                                        ))
+                                        .taxes(
+                                                List.of(PartnerReservationOrdersPriceDescriptionTaxes.builder()
+                                                        .amount(new BigDecimal(10))
+                                                        .description("TESTE_TAX")
+                                                        .build())).build()
+                        ).build(),
                 PricingCalculatePrice.builder().priceListId("price")
                         .pricesDescription(PricingCalculatePricesDescription.builder()
                                 .flights(List.of(PricingCalculateFlight.builder()
-                                                .passengerType("BY_ADULT")
-                                                .pointsAmount(new BigDecimal(10))
+                                        .passengerType("BY_ADULT")
+                                        .pointsAmount(new BigDecimal(10))
                                         .build()))
 
                                 .taxes(List.of(PricingCalculateTaxes.builder()
                                         .type("TESTE_TAX")
-                                                .pointsAmount(new BigDecimal(7))
+                                        .pointsAmount(new BigDecimal(7))
                                         .build()))
                                 .build()).build());
         assertNotNull(response);
