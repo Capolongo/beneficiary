a5316698c270a4949e23377159c17d60269a72bc
T01400 <renan.melo@compasso.com.br>
2024-03-04 17:24:19 -0300

    refactor: voltando as configs de antes

--- src/main/java/br/com/livelo/orderflight/mappers/ReservationTravelInfoEntityMapper.java
+++ src/main/java/br/com/livelo/orderflight/mappers/ReservationTravelInfoEntityMapper.java
@@ -1,37 +1,22 @@
 package br.com.livelo.orderflight.mappers;
 
-import br.com.livelo.orderflight.domain.dto.reservation.request.ReservationPax;
 import br.com.livelo.orderflight.domain.dto.reservation.request.ReservationRequest;
 import br.com.livelo.orderflight.domain.dto.reservation.response.PartnerReservationTravelInfo;
-import br.com.livelo.orderflight.domain.entity.PaxEntity;
 import br.com.livelo.orderflight.domain.entity.TravelInfoEntity;
 import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
-import org.mapstruct.factory.Mappers;
 
-import java.util.List;
 import java.util.Objects;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 
-@Mapper(componentModel = "spring", uses = {ReservationPaxMap.class})
+@Mapper(componentModel = "spring")
 
 public interface ReservationTravelInfoEntityMapper {
 
-    @Mapping(target = "paxs", expression = "java(mapPax(reservationRequest))")
+    @Mapping(target = "paxs", source = "reservationRequest.paxs")
     TravelInfoEntity toReservationTravelInfoEntity(ReservationRequest reservationRequest, PartnerReservationTravelInfo partnerReservationTravelInfo);
-
-
-    default Set<PaxEntity> mapPax(ReservationRequest reservationRequest) {
-        var mapperPax = Mappers.getMapper(ReservationPaxMap.class);
-
-        List<ReservationPax> paxs = reservationRequest.getPaxs();
-
-        return paxs.stream().map(mapperPax::toPaxEntity).collect(Collectors.toSet());
-    }
 
     @AfterMapping
     default void setType(PartnerReservationTravelInfo partnerReservationTravelInfo, @MappingTarget TravelInfoEntity travelInfoEntity) {
