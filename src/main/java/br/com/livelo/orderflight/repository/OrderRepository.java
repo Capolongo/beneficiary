83be712fe37552fb1b06705d6be0c3ad2ad9c143
Joao Polo <joao.polo@ilia.digital>
2024-03-11 14:54:37 -0300

    (code review) adjust repository sql location

--- src/main/java/br/com/livelo/orderflight/repository/OrderRepository.java
+++ src/main/java/br/com/livelo/orderflight/repository/OrderRepository.java
@@ -6,7 +6,6 @@ import br.com.livelo.orderflight.domain.entity.OrderEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -16,14 +15,6 @@ public interface OrderRepository extends JpaRepository<OrderEntity, String> {
 
         Page<OrderProcess> findAllByCurrentStatusCode(String statusCode, Pageable pageable);
 
-        @Query("select new br.com.livelo.orderflight.domain.dtos.repository.OrderProcess(o.id, o.commerceOrderId) " +
-                        " from OrderEntity o " +
-                        " inner join o.currentStatus os " +
-                        " inner join o.items oi " +
-                        " inner join oi.segments s " +
-                        " where os.code = ?1 " +
-                        " group by o.id, o.commerceOrderId " +
-                        " having max(s.arrivalDate) < ?2 ")
         Page<OrderProcess> findAllByCurrentStatusCodeAndArrivalDateLessThan(String statusCode,
                         LocalDateTime expirationDate, Pageable pageable);
 
