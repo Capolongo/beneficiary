0ea77c538753bcdacbfb2676ab97f0f13d9a3e91
Kaê Benning <kae.leal@taking.com.br>
2024-04-18 17:45:40 -0300

    fix: change addPartnerLink order

--- src/main/java/br/com/livelo/orderflight/service/reservation/impl/ReservationServiceImpl.java
+++ src/main/java/br/com/livelo/orderflight/service/reservation/impl/ReservationServiceImpl.java
@@ -88,11 +88,12 @@ public class ReservationServiceImpl implements ReservationService {
 
             if (this.isNewOrder(order)) {
                 order = reservationMapper.toOrderEntity(request, partnerReservationResponse, transactionId, customerId, channel, listPriceId);
-                addPartnerOrderLinkIdToItems(order.getPartnerCode(), order.getCommerceOrderId(), order.getItems());
             }
 
             var pricingCalculatePrice = this.priceOrder(request, partnerReservationResponse);
             this.setPrices(order, pricingCalculatePrice, listPriceId);
+
+            addPartnerOrderLinkIdToItems(order.getPartnerCode(), order.getCommerceOrderId(), order.getItems());
 
             this.orderService.save(order);
 
