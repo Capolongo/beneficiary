577be6c4bda9aef95a83aceae054682ff4afad26
mariomendonca <mario.neto@taking.com.br>
2024-06-04 13:46:45 -0300

    fix: remove not used param

--- src/main/java/br/com/livelo/orderflight/repository/OrderRepository.java
+++ src/main/java/br/com/livelo/orderflight/repository/OrderRepository.java
@@ -5,7 +5,6 @@ import br.com.livelo.orderflight.domain.entity.OrderEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
