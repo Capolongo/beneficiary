d270f1798580f00b979b939998801a3bb7f0b043
Jose Robson <jose.lima@taking.com.br>
2024-06-05 11:31:47 -0300

    Ajustando imports

--- src/main/java/br/com/livelo/orderflight/domain/dto/reservation/request/PartnerReservationDocument.java
+++ src/main/java/br/com/livelo/orderflight/domain/dto/reservation/request/PartnerReservationDocument.java
@@ -1,6 +1,5 @@
 package br.com.livelo.orderflight.domain.dto.reservation.request;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
