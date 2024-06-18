bf00d78129e1a55d332b1601f4257e4962aa21e6
9002949 <31152127@Fb1>
2024-06-18 16:25:43 -0300

    Ajuste teste log pointsAmount

--- src/test/java/br/com/livelo/orderflight/controller/ReservationControllerTest.java
+++ src/test/java/br/com/livelo/orderflight/controller/ReservationControllerTest.java
@@ -2,9 +2,8 @@ package br.com.livelo.orderflight.controller;
 
 import br.com.livelo.orderflight.domain.dto.reservation.request.ReservationRequest;
 import br.com.livelo.orderflight.domain.dto.reservation.response.ReservationResponse;
-import br.com.livelo.orderflight.domain.entity.OrderEntity;
+import br.com.livelo.orderflight.domain.dto.reservation.response.ReservationResponsePrice;
 import br.com.livelo.orderflight.service.reservation.impl.ReservationServiceImpl;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -13,9 +12,10 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @ExtendWith({MockitoExtension.class})
 class ReservationControllerTest {
@@ -28,7 +28,9 @@ class ReservationControllerTest {
     void shouldCreateReservation() {
         var expected = mock(ReservationResponse.class);
         var request = mock(ReservationRequest.class);
+        var priceMock = mock(ReservationResponsePrice.class);
         Mockito.when(reservationService.createOrder(any(ReservationRequest.class), anyString(), anyString(), anyString(), anyString(), anyString())).thenReturn(expected);
+        when(expected.price()).thenReturn(priceMock);
 
         var response = this.reservationController.createReservation("123", "123", "123", "WEB", "price", request);
         assertAll(
